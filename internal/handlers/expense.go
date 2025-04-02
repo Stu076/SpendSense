0cb52b1a4e14e98935700e225eb14404d10e8580
Jan Stupica <stupicajan@gmail.com>
2025-04-02 16:02:17 +0200

    Add Redis Cache for Expenses

--- internal/handlers/expense.go
+++ internal/handlers/expense.go
@@ -2,9 +2,13 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/Stu076/SpendSense/internal/cache"
 	"github.com/Stu076/SpendSense/internal/models"
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
@@ -13,6 +17,8 @@ import (
 type ExpenseHandler struct {
 	DB *bun.DB
 }
+
+// TODO: add UPDATE expense and get expense by ID
 
 func (h *ExpenseHandler) CreateExpense(c *gin.Context) {
 	username, exists := c.Get("username")
@@ -42,6 +48,13 @@ func (h *ExpenseHandler) CreateExpense(c *gin.Context) {
 		return
 	}
 
+	// Delete from Cache
+	err = cache.DeleteCache("expenses:" + strconv.Itoa(user.ID))
+	if err != nil {
+		log.Println("❌ Failed to delete cache:", err)
+	}
+	// TODO: set cache for the new expense
+
 	c.JSON(http.StatusCreated, gin.H{"message": "Expense created successfully", "expense": expense})
 }
 
@@ -60,10 +73,40 @@ func (h *ExpenseHandler) GetExpenses(c *gin.Context) {
 		return
 	}
 
+	cacheKey := "expenses:" + strconv.Itoa(user.ID)
+	cachedExpenses, err := cache.GetCache(cacheKey)
+
+	// Cache hit
+	if err == nil {
+		log.Println("✅ Serving from Redis Cache")
+		var expenses []models.Expense
+		err = json.Unmarshal([]byte(cachedExpenses), &expenses)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse cached expenses"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"expenses": expenses})
+		return
+	}
+
+	// Cache miss
 	var expenses []models.Expense
 	err = h.DB.NewSelect().Model(&expenses).Where("user_id = ?", user.ID).Order("created_at DESC").Scan(context.Background())
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch expenses"})
+		return
+	}
+
+	// Set cache
+	expensesJSON, err := json.Marshal(expenses)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to cache expenses"})
+		return
+	}
+
+	err = cache.SetCache(cacheKey, string(expensesJSON), 10*time.Minute)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to cache expenses"})
 		return
 	}
 
@@ -99,6 +142,13 @@ func (h *ExpenseHandler) DeleteExpense(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not delete expense"})
 		return
 	}
+
+	// Delete from Cache
+	err = cache.DeleteCache("expenses:" + strconv.Itoa(user.ID))
+	if err != nil {
+		log.Println("❌ Failed to delete cache:", err)
+	}
+	// TODO: delete cache for the deleted expense
 
 	c.JSON(http.StatusOK, gin.H{"message": "Expense deleted"})
 }
